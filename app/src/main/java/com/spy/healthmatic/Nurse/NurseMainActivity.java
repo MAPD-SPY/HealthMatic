dc09bf800390de5873c3a58fbc6d8952b3037e98
prashant pol <prashantn.pol@gmail.com>
2016-10-31 15:41:28 -0400

    Nurse Module Added

---
+++ app/src/main/java/com/spy/healthmatic/Nurse/NurseMainActivity.java
@@ -0,0 +1,15 @@
+package com.spy.healthmatic.Nurse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spy.healthmatic.R;
+
+public class NurseMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nurse_main);
+    }
+}
