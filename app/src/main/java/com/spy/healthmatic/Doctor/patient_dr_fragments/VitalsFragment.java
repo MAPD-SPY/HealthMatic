d378b4ea3f09177ccf1f8d17d4dad6647102ff1c
shelalaine <schan211@my.centennialcollege.ca>
2016-11-02 19:34:10 -0400

    lisitng and detail of patient done

---
+++ app/src/main/java/com/spy/healthmatic/Doctor/patient_dr_fragments/VitalsFragment.java
@@ -0,0 +1,63 @@
+package com.spy.healthmatic.Doctor.patient_dr_fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.spy.healthmatic.R;
+
+/**
+ * Team Name: Team SPY
+ * Created by shelalainechan on 2016-11-01.
+ */
+public class VitalsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public VitalsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VitalsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VitalsFragment newInstance(String param1, String param2) {
+        VitalsFragment fragment = new VitalsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_vitals, container, false);
+        return view;
+    }
+}
