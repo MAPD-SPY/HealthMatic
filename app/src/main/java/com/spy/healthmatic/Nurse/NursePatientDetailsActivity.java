0bbff3d1bb3bbe329848ea8d7357d781a72a27a1
Yatin Sharma <yatin.sharma1991@gmail.com>
2016-11-04 18:27:02 -0400

    Nurse view corrections

--- app/src/main/java/com/spy/healthmatic/Nurse/NursePatientDetailsActivity.java
+++ app/src/main/java/com/spy/healthmatic/Nurse/NursePatientDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.spy.healthmatic.Nurse;
 
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -88,7 +89,10 @@ public class NursePatientDetailsActivity extends AppCompatActivity {
         title.setTextAppearance(this, android.R.style.TextAppearance_Material_Widget_ActionBar_Title_Inverse);
         toolbar.addView(title);
 
-
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsetoolbarPatientDr);
+        collapsingToolbarLayout.setTitle("");
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(android.R.color.transparent));
     }
 
 
