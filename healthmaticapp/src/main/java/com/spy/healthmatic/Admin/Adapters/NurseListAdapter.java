fe248ee79e9c9a47c9184814435cf2fb632616f8
Yatin Sharma <yatin.sharma1991@gmail.com>
2016-12-13 00:51:24 -0500

    Photo upload completed

--- healthmaticapp/src/main/java/com/spy/healthmatic/Admin/Adapters/NurseListAdapter.java
+++ healthmaticapp/src/main/java/com/spy/healthmatic/Admin/Adapters/NurseListAdapter.java
@@ -1,5 +1,6 @@
 package com.spy.healthmatic.Admin.Adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +8,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.spy.healthmatic.Admin.Fragments.NurseList;
 import com.spy.healthmatic.Model.Nurse;
 import com.spy.healthmatic.Model.Staff;
 import com.spy.healthmatic.R;
 
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 
 /**
  * Created by yatin on 28/10/16.
@@ -22,10 +25,12 @@ public class NurseListAdapter extends RecyclerView.Adapter<NurseListAdapter.View
 
     private ArrayList<Staff> nurses;
     private final NurseList.OnNurseListFragmentInteractionListener mListener;
+    private Context mContext;
 
-    public NurseListAdapter(ArrayList<Staff> nurses, NurseList.OnNurseListFragmentInteractionListener listener) {
+    public NurseListAdapter(ArrayList<Staff> nurses, NurseList.OnNurseListFragmentInteractionListener listener, Context context) {
         this.nurses = nurses;
         mListener = listener;
+        mContext = context;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -58,10 +63,14 @@ public class NurseListAdapter extends RecyclerView.Adapter<NurseListAdapter.View
         holder.mNameView.setText(nurse.getFirstName());
         holder.mPateintConditionView.setText(nurse.getLastName());
         holder.mRoomNumberView.setText(nurse.getFloor()+"");
-        if("male".equals(nurse.getGender()+"")){
-            holder.mPatientGenderIdicator.setImageResource(R.drawable.user_male);
-        }else {
-            holder.mPatientGenderIdicator.setImageResource(R.drawable.user_female);
+        if (nurse.getImageName() != null && !"".equals(nurse.getImageName())) {
+            Glide.with(mContext).load(nurse.getImageName()).error(R.drawable.ic_menu_camera).into(holder.mPatientGenderIdicator);
+        } else {
+            if ("male".equals(nurse.getGender() + "")) {
+                holder.mPatientGenderIdicator.setImageResource(R.drawable.user_male);
+            } else {
+                holder.mPatientGenderIdicator.setImageResource(R.drawable.user_female);
+            }
         }
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
